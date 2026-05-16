ab2673ca7d823cc8408c654b2c3cb2df16e72b03
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-05-16 19:59:20 +0200

    Fix base path per pagina bianca

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solidPlugin()],
+  base: '/dolce-solid/', // <--- Questa riga dice a Vite che il sito si trova dentro la cartella di GitHub Pages!
+  build: {
+    target: 'esnext',
+  },
+});
