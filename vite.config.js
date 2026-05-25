8b81aacc1880b4ab0814572db489f6eaab7eaa80
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-05-25 23:28:36 +0200

    mod on vite for cloudflare hosting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/dolce-solid/', // <--- Questa riga dice a Vite che il sito si trova dentro la cartella di GitHub Pages!
+  base: './', // <--- Questa riga dice a Vite che il sito si trova dentro la cartella di GitHub Pages!
   build: {
     target: 'esnext',
   },
