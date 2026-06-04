3ae6a1e6753c90881bb3047ba6a7348fc7299e48
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-04 08:51:52 +0200

    parallax.js new

--- src/Parallax.js
+++ src/Parallax.js
@@ -1,6 +1,8 @@
-window.addEventListener('scroll', () => {
-  const parallaxBands = document.querySelectorAll('.parallax-band');
-  parallaxBands.forEach(band => {
-    band.style.backgroundPosition = `center ${window.scrollY * 0.5}px`;
+export function initParallax() {
+  window.addEventListener('scroll', () => {
+    const parallaxBands = document.querySelectorAll('.parallax-band');
+    parallaxBands.forEach(band => {
+      band.style.backgroundPosition = `center ${window.scrollY * 0.5}px`;
+    });
   });
-});
+}
