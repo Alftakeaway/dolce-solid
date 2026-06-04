725538e97ce370dab556d93ef08951d29e87ea92
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-04 07:55:44 +0200

    added native animation

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { render } from 'solid-js/web';
 import App from './App';
+import { withAnimations } from './withAnimations';
+
+const AnimatedApp = withAnimations(App);
 
 const root = document.getElementById('root');
-render(() => <App />, root);
+render(() => <AnimatedApp />, root);
