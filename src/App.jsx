c902c06de1737609e84ba581fe69498941bb4656
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-04 19:03:26 +0200

    menu true

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
       <AboutSection />
 
       {/* MENU SECTION - EXTRACTED COMPONENT - DORMIENTE FINCHÉ NON ARRIVANO LE FOTO */}
-      {false && <MenuSection menuItems={menuItems} />}
+      {true && <MenuSection menuItems={menuItems} />}
 
       {/* PARALLAX BAND 1 */}
       <div class="parallax-band parallax-band-1">
