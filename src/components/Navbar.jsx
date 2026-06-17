6ba6f78284889ee50cd2283b9ac4758ad798c24a
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-17 11:03:18 +0200

    menu on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,36 @@
+import { createSignal } from "solid-js";
+
 function Navbar() {
+  //  Interruttore true/false
+  const [isOpen, setIsOpen] = createSignal(true);
+
   return (
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#home">
           <img src="assets/dolce_vita_logo_no_bg1.png" alt="Dolce Vita" class="navbar-logo" />
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        
+        {/* Al click invertiamo il valore dell'interruttore */}
+        <button 
+          class="navbar-toggler" 
+          type="button" 
+          onClick={() => setIsOpen(!isOpen())}
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        
+        {/* Se isOpen è true, Solid aggiunge la classe 'show' che apre il menu */}
+        <div class={`collapse navbar-collapse ${isOpen() ? 'show' : ''}`} id="navbarNav">
           <ul class="navbar-nav ms-auto">
-            <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="#about">Our Story</a></li>
-            <li class="nav-item"><a class="nav-link" href="#menu">Menu</a></li>
-            <li class="nav-item"><a class="nav-link" href="#gallery">Atmosphere</a></li>
-            <li class="nav-item"><a class="nav-link" href="#reviews">Reviews</a></li>
-            <li class="nav-item"><a class="nav-link" href="#reservation">Book a Table</a></li>
-            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            {/* Aggiungendo onClick={() => setIsOpen(false)} il menu si chiude appena clicchi la voce! */}
+            <li class="nav-item"><a class="nav-link" href="#home" onClick={() => setIsOpen(false)}>Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about" onClick={() => setIsOpen(false)}>Our Story</a></li>
+            <li class="nav-item"><a class="nav-link" href="#menu" onClick={() => setIsOpen(false)}>Menu</a></li>
+            <li class="nav-item"><a class="nav-link" href="#gallery" onClick={() => setIsOpen(false)}>Atmosphere</a></li>
+            <li class="nav-item"><a class="nav-link" href="#reviews" onClick={() => setIsOpen(false)}>Reviews</a></li>
+            <li class="nav-item"><a class="nav-link" href="#reservation" onClick={() => setIsOpen(false)}>Book a Table</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
           </ul>
         </div>
       </div>
