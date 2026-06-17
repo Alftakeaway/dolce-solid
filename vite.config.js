24d235b0725cc2140ba231bfcb718a78519af46e
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-17 14:24:52 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: './', // <--- Questa riga dice a Vite che il sito si trova dentro la cartella di GitHub Pages!
   build: {
     target: 'esnext',
   },
