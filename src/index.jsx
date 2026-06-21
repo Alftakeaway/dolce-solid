d062a90cbd0da6fdeac72506810362e074d4152f
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-06-21 22:26:00 +0200

    hhh

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,20 @@ import { withAnimations } from './withAnimations';
 
 const AnimatedApp = withAnimations(App);
 
-// Parallax inline
-window.addEventListener('scroll', () => {
+// Parallax
+const initParallax = () => {
   const parallaxBands = document.querySelectorAll('.parallax-band');
   parallaxBands.forEach(band => {
-    band.style.backgroundPosition = `center bottom ${window.scrollY * -0.5}px`;
+    if (band.classList.contains('parallax-band-1')) {
+      band.style.backgroundPosition = `center bottom ${window.scrollY * -0.5}px`;
+    } else {
+      band.style.backgroundPosition = `center ${window.scrollY * 0.5}px`;
+    }
   });
-});
+};
+
+initParallax();
+window.addEventListener('scroll', initParallax);
 
 const root = document.getElementById('root');
 render(() => <AnimatedApp />, root);
